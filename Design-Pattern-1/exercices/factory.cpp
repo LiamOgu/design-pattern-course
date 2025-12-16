021af4a94bed7c281f4947b4a440c6245b8c01b3
LiamOgu <augu.liam@gmail.com>
2025-12-16 21:30:35 +0100

    finission de factory

--- Design-Pattern-1/exercices/factory.cpp
+++ Design-Pattern-1/exercices/factory.cpp
@@ -31,3 +31,29 @@ public:
     soldat->combattre();
   }
 };
+
+class CaserneHumaine : public Caserne {
+public:
+  std::unique_ptr<Unite> creerUnite() const override {
+    return std::make_unique<Archer>();
+  }
+};
+
+class CaserneRobot : public Caserne {
+public:
+  std::unique_ptr<Unite> creerUnite() const override {
+    return std::make_unique<Terminator>();
+  }
+};
+
+int main() {
+  std::unique_ptr<Caserne> caserne;
+
+  caserne = std::make_unique<CaserneHumaine>();
+  caserne->entrainer();
+
+  caserne = std::make_unique<CaserneRobot>();
+  caserne->entrainer();
+
+  return 0;
+}
